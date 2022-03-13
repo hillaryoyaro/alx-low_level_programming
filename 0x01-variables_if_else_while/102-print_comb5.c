396d9c04837a9f452e988e1c38c6517df798f7bb
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-03-13 00:11:33 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,17 @@
 #include<stdio.h>
 int main(void)
 {
-int x, y;
-for (x = 0; x <= 98; x++)
+int num1, num2;
+for (num1 = 0; num1 <= 98; num1++)
 {
-	for (y = x + 1; y <= 99; y++)
+	for (num2 = num1 + 1; num2 <= 99; num2++)
 	{
-		putchar((x / 10) + '0');
-		putchar((x % 10) + '0');
+		putchar((num1 / 10) + '0');
+		putchar((num1 % 10) + '0');
 		putchar(' ');
-		putchar((y / 10) + '0');
-		putchar((y % 10) + '0');
-		if (x == 98 && y == 99)
+		putchar((num2 / 10) + '0');
+		putchar((num2 % 10) + '0');
+		if (num1 == 98 && num2 == 99)
 		{
 			continue;
 			putchar(',');
