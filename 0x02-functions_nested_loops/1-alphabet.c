035cb093b137bea1ce3b4433377e8661a12f7e55
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-03-15 15:05:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+/*
+ *
+ * File: 1-alphabet.c
+ * Auth: Hillary Oyaro
+ */
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0
+*/
+void print_alphabet(void)
+
+{
+
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
