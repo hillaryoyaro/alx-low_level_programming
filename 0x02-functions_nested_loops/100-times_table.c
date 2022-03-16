3021bf8b838c3fa318372f641b27947abd184df2
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-03-16 03:43:24 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,7 +21,7 @@ void print_times_table(int n)
 			_putchar('0');
 			for (mult =1 ; mult <=n ; mult++)
 			{
-				_putchar (',');
+				_putchar(',');
 				_putchar(' ');
 				prod = mult * num;
 				if (prod <= 99)
