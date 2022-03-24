7247e12cba93debb4714f091c390613217dbd011
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-03-24 10:37:28 -0700

    a function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+/*
+ * Auth:Hillary Oyaro
+ * File:2-strncpy.c
+ */
+#include "main.h"
+/**
+* _strncpy - copies a string
+* @dest: string to copy to
+* @src: string to copy from
+* @n: maximum number of characters to copy from src
+*
+* Description:
+* works exactly like strncpy from <string.h>
+*
+* Return: a pointer to the resulting string dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *mydest = dest;
+
+	/* copy at most n characters to dest */
+	while (*src && (i < n))
+	{
+		*dest++ = *src++;
+		i++;
+	}
+
+	/* ensure than n bytes are written */
+	while (i < n)
+	{
+		*dest++ = '\0';
+		i++;
+	}
+
+	return (mydest);
+
+}
