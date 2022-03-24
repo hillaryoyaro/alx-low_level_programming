faace9be8ceb511ede2d88052948fc2652c94eb0
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-03-24 11:32:17 -0700

    a function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+/*
+ * Auth:Hillary Oyaro
+ * File:100-rot13.c
+ */
+#include "main.h"
+/**
+* rot13 -  encodes a string using rot13
+* @str: string to be encoded
+*
+* Return: pointer to the encoded string
+*/
+char *rot13(char *str)
+{
+	/* conversion tables */
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char values[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = values[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
