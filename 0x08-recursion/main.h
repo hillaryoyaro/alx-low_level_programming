3884399c9667b506830d82af6d5d6c8199839160
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-03-30 03:51:50 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,14 @@
+#ifndef ALX
+#define ALX
+
+/**
+ * File: main.h
+ * Auth: Hillary Oyaro
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x08-recursion directory.
+ */
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -5,3 +16,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
