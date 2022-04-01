08b096319d397c485170d9b3f5c98b5409afb8b2
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-04-01 02:12:06 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+/*
+ * Auth:Hillary Oyaro
+ * File:0-whatsmyname.c
+ */
+#include<stdio.h>
+/**
+ * main - prints its name + \n
+ * @argc: number of arguments typed
+ * @argv: array pointing to arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
