4011361b4509abea88a6983a62d91edc5d2d7bfd
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-04-07 08:28:02 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,6 @@
 #ifndef ALX_H
 #define ALX_H
 
-/**
- * File: main.h
- * Auth: Hillary Oyaro
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0C-more_malloc_free directory.
- */
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
