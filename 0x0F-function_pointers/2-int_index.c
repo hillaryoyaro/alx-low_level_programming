38e095f60034d161f483f314fa4900caefaa9dae
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-04-13 04:24:38 -0700

    searching for integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+/*
+ * Auth:Hillary Oyaro
+ * File:2-int_index.c
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
+}
