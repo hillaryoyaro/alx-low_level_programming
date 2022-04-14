99b9e1ee5a82e6add451323d29a524ddce898a28
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-04-14 12:52:17 -0700

    Sum of its parameter.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
  * File:0-sum_them_all.c
  */
 #include<stdio.h>
-#include <stdarg.h>
+#include<stdarg.h>
 #include "variadic_functions.h"
 /**
  * sum_them_all - Entry Point
@@ -12,17 +12,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list num;
-	unsigned int i, sum = 0;
+	va_list nums;
+	unsigned int index, sum = 0;
 
-	if (n == 0)
-		return (0);
+	va_start(nums, n);
 
-	va_start(num, n);
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
 
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(num, const unsigned int);
-	}
+	va_end(nums);
 
-	va_end(num);
+	return (sum);
+}
